7040a5c209450ddb482280519d7a60e55e2fceb4
genesis <hmorteza85@gmail.com>
2019-05-15 19:58:27 +0430

    addition forms.py and urls.py in addresses app and edit all files in this app and create templates directory and sub html file and edit it

--- addresses/models.py
+++ addresses/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from billing.models import BillingProfile
 
-# Create your models here.
+ADDRESS_TYPES = (
+    ('billing', 'Billing'),
+    ('shipping', 'Shipping'),
+)
+
+
+class Address(models.Model):
+    billing_profile = models.ForeignKey(BillingProfile, on_delete=models.CASCADE)
+    address_type = models.CharField(max_length=150, choices=ADDRESS_TYPES)
+    address_line_1 = models.CharField(max_length=150)
+    address_line_2 = models.CharField(max_length=150, null=True, blank=True)
+    country = models.CharField(max_length=150)
+    city = models.CharField(max_length=150)
+    state = models.CharField(max_length=150)
+    postal_code = models.CharField(max_length=150, null=True, blank=True)
+
+    def __str__(self):
+        return self.postal_code
+
+    def get_address(self):
+        return "{line1}\n{line2}\n{city}\n{state}, {postal}\n{country}".format(
+            line1=self.address_line_1,
+            line2=self.address_line_2 or "",
+            city=self.city,
+            state=self.state,
+            postal=self.postal_code,
+            country=self.country
+        )
--- addresses/views.py
+++ addresses/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils.http import is_safe_url
+from django.views.decorators.csrf import csrf_protect
+from .forms import AddressForm
+from .models import Address
+from billing.models import BillingProfile
 
-# Create your views here.
+
+@csrf_protect
+def checkout_address_create_view(request):
+    form = AddressForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    next_ = request.GET.get('next')
+    next_post = request.POST.get('next')
+    redirect_path = next_ or None
+    if form.is_valid():
+        instance = form.save(commit=False)
+        billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+        if billing_profile is not None:
+            address_type = request.POST.get('address_type', 'address')
+            instance.billing_profile = billing_profile
+            instance.address_type = address_type
+            instance.save()
+
+            request.session[address_type + "_address_id"] = instance.id
+            shipping_address_id = request.session.get("shipping_address_id", None)
+            billing_address_id = request.session.get("billing_address_id", None)
+        else:
+            print("Goddammit Error is happen")
+            return redirect("carts:checkout")
+
+        if is_safe_url(redirect_path, request.get_host()):
+            return redirect(redirect_path)
+    return render(request, "addresses/form.html", context)
+
+
+@csrf_protect
+def checkout_address_reuse_view(request):
+    if request.user.is_authenticated:
+        next_ = request.GET.get('next')
+        next_post = request.POST.get('next')
+        redirect_path = next_ or None
+
+        if request.method == "POST":
+            shipping_address = request.POST.get('shipping_address', None)
+            address_type = request.POST.get('address_type', 'address')
+            billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+            if shipping_address is not None:
+                qs = Address.objects.filter(billing_profile=billing_profile, id=shipping_address)
+                if qs.exists():
+                    request.session[address_type + "_address_id"] = shipping_address
+                if is_safe_url(redirect_path, request.get_host()):
+                    return redirect(redirect_path)
+    return redirect("carts:checkout")
