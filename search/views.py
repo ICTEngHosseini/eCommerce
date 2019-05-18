84382dab3f74ce3c439e41f09805c5b9a1a606a1
genesis <hmorteza85@gmail.com>
2019-05-18 14:51:31 +0430

    add urls.py and templates dir and sub html files in there and edit it all and edit views.py

--- search/views.py
+++ search/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from products.models import Product
 
-# Create your views here.
+class SearchProductView(ListView):
+    template_name = "search/search.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(SearchProductView, self).get_context_data(*args, **kwargs)
+        query = self.request.GET.get('q')
+        context['query'] = query
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        query = request.GET.get('q', None)
+        if query is not None:
+            return Product.objects.search(query)
+        else:
+            return Product.objects.none
+
+        '''
+        __icontains = field contains this
+        __iexact = field is exactly this
+
+        '''
