5aaa803d454293432c7da2b9c179014674bfec77
genesis <hmorteza85@gmail.com>
2019-05-18 14:05:49 +0430

    i forget edit admin file in analytics app now i edited

--- analytics/admin.py
+++ analytics/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ObjectViewed, UserSession
+
+admin.site.register(ObjectViewed)
+admin.site.register(UserSession)
