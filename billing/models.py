ebf0d9363730a6095f7ed431b4fd43296c1dcf9a
genesis <hmorteza85@gmail.com>
2019-05-18 14:13:21 +0430

    now edit admin.py and models.py and noting have to do :)

--- billing/models.py
+++ billing/models.py
@@ -1,3 +1,63 @@
+import stripe
 from django.db import models
+from django.conf import settings
+from django.db.models.signals import post_save, pre_save
 
-# Create your models here.
+from accounts.models import GuestEmail
+
+User = settings.AUTH_USER_MODEL
+stripe.api_key = "sk_test_xsB0ceSWejUXhmSJSlOnqitJ00TTZHqDU5"
+
+
+class BillingProfileManager(models.Manager):
+    def new_or_get(self, request):
+        user = request.user
+        guest_email_id = request.session.get('guest_email_id')
+        created = False
+        obj = None
+        if user.is_authenticated:
+            obj, created = self.model.objects.get_or_create(
+                user=user, email=user.email)
+        elif guest_email_id is not None:
+            guest_email_obj = GuestEmail.objects.get(id=guest_email_id)
+            obj, created = self.model.objects.get_or_create(
+                email=guest_email_obj.email)
+        else:
+            pass
+        return obj, created
+
+
+class BillingProfile(models.Model):
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=254)
+    active = models.BooleanField(default=True)
+    update = models.TimeField(auto_now=True)
+    timestamp = models.TimeField(auto_now_add=True)
+    customer_id = models.CharField(max_length=120, null=True, blank=True)
+    # customer_id in Stripe or Braintree
+
+    objects = BillingProfileManager()
+
+    def __str__(self):
+        return self.email
+
+
+def billing_profile_created_receiver(sender, instance, *args, **kwargs):
+    if not instance.customer_id and instance.email:
+        print("ACTUAL API REQUEST Send to stripe")
+        customer = stripe.Customer.create(
+            email=instance.email
+        )
+        print(customer)
+        instance.customer_id = customer.id
+
+
+pre_save.connect(billing_profile_created_receiver, sender=BillingProfile)
+
+
+def user_created_receiver(sender, instance, created, *args, **kwargs):
+    if created:
+        BillingProfile.objects.get_or_create(user=instance, email=instance.email)
+
+
+post_save.connect(user_created_receiver, sender=User)
